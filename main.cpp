92559272f7e2c2e2b34ef04c91725fffcca1dcb2
Majkel44 <m.werner9205@gmail.com>
2016-05-10 19:52:52 +0200

    v 0.1.0 - added method for selecting device - added method for checking connection status - added method for getting data from device

--- main.cpp
+++ main.cpp
@@ -1,21 +1,27 @@
 #include <QCoreApplication>
 #include <QDebug>
 #include <iostream>
-#include "modbus.h"
-#include "modbus-private.h"
+
 #include "modbus_communicator.h"
 
 using namespace std;
-modbus_t* my_modbus;
+
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
     char status_buffer[128];
-
+    int status;
+    uint16_t data[1024];
     modbus_communicator mb;
 
-    mb.connect_to_modbus_network("COM10",9600,'E',8,1);
+    status =  mb.check_and_connect_to_modbus_network("COM9",9600,'E',8,1);
+    qDebug() << "Status: " << status;
+    status = mb.check_and_get_data_from_device(9,4,1000,data);
+
+
+
+
 
 
 
--- modbus_communicator.h
+++ modbus_communicator.h
@@ -1,5 +1,10 @@
 #ifndef MODBUS_COMMUNICATOR_H
 #define MODBUS_COMMUNICATOR_H
+
+#define NO_CONNECT 0
+#define CONNECTION_ERROR -1
+#define CONNECTION_OK 1
+
 #include "modbus.h"
 #include <QDebug>
 
@@ -9,12 +14,12 @@ class modbus_communicator{
 public:
     modbus_communicator();
 
-    int connect_to_modbus_network(char *com_port, int baud_rate, char parity, int data_length, int stop_bits);
-//    int select_device(int device_address);
-//    int get_data_from_device(int device, int * data);
+    int check_and_connect_to_modbus_network(char *com_port, int baud_rate, char parity, int data_length, int stop_bits);
+    int check_and_select_device(int device_address);
+    int check_and_get_data_from_device(int device, int quanity, int addr, uint16_t * data);
 
 private:
-
+    int check_connection_status();
     modbus_t* mb_port;
 
 
