96b6a7d7da88e39f6d32f618b0c76b07e1634ff6
Majkel44 <m.werner9205@gmail.com>
2016-05-17 18:26:26 +0200

    - implementing of modbus and db layer (inserting data from agents to db)

--- ems_supervisor_main_class.cpp
+++ ems_supervisor_main_class.cpp
@@ -3,10 +3,10 @@
 ems_supervisor_main_class::ems_supervisor_main_class()
 {
 
-  timer = new QTimer(this);
-  connect(timer,SIGNAL(timeout()),this,SLOT(agents_pool_update()));
-  mb.check_and_connect_to_modbus_network("COM9",9600,'E',8,1);
-  ems_db.open_db("localhost","ems","root","");
+    timer = new QTimer(this);
+    connect(timer,SIGNAL(timeout()),this,SLOT(agents_pool_update()));
+    mb.check_and_connect_to_modbus_network("COM9",9600,'E',8,1);
+    ems_db.open_db("localhost","ems","root","");
 
 
 
@@ -33,6 +33,7 @@ void ems_supervisor_main_class::process_energy_consumption_data(uint16_t *energy
     int energy_consumption = energy_consumption_array[0];
 
     ems_db.insert_energy_consumption_int_db(energy_consumption);
+    qDebug () << "R1[" << "0" << "]: "<< energy_consumption_array[0];
 
 }
 
@@ -41,8 +42,11 @@ void ems_supervisor_main_class::process_temperature_data(uint16_t *temperature_a
     for(int register_adress = 0; register_adress<8; register_adress++){
 
         int sensor_id = temperature_array[register_adress];
-        int temperature = temperature_array[register_adress];
+        int temperature = temperature_array[register_adress+8];
         ems_db.insert_temperatures_into_db(QString::number(sensor_id),temperature);
+
+        qDebug () << register_adress << "ID: "<< sensor_id << "|" << "Temperatura: " << temperature;
+
 
 
     }
@@ -57,22 +61,18 @@ void ems_supervisor_main_class::process_temperature_data(uint16_t *temperature_a
 
 void ems_supervisor_main_class::agents_pool_update(){
 
-//  mb.check_and_get_data_from_device(9,4,1000,slave_1);
-//  mb.check_and_get_data_from_device(10,4,1000,slave_2);
+    int status2 =  mb.check_and_get_data_from_device(9,4,1000,slave_2);
+    int status =  mb.check_and_get_data_from_device(10,16,1000,slave_1);
+    qDebug () << "Slave: 10" <<  status;
+    //qDebug () << "Slave: 9" << status2;
 
-//  for(int i=0; i<4; i++){
-
-//      qDebug () << "R1" << "[ " << i << "]" << ": " << slave_1[i];
-
-
-//  }
-
-//  for(int i=0; i<4; i++){
-
-//      qDebug () << "R2" << "[ " << i << "]" << ": " << slave_2[i];
+    if(status!=-1){
+        process_temperature_data(slave_1);
+        process_energy_consumption_data(slave_2);
+    }
 
 
-//  }
+
 
 
 }
